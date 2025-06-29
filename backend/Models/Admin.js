22bbcf79cbb704050ed3979cd2d94e10b4da18c3
Alla Trivikram <allatrivikram@gmail.com>
2025-06-29 20:20:57 +0530

    Delete backend directory

--- backend/Models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-const adminSchema=new mongoose.Schema({
-    email:String,
-    password:String, 
-   
-})
-const Model=mongoose.model("Admin",adminSchema)
-module.exports=Model
